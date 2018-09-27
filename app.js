b95fbb923a5bb5b60bdb83e035ae5481af756a9a
Ubuntu <ubuntu@ip-172-31-44-201.us-east-2.compute.internal>
2018-09-27 16:13:59 +0000

    부트스트랩 modal 추가

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require('express');   
 var app = express();
 const bodyParser = require('body-parser');
 const session = require('express-session');
